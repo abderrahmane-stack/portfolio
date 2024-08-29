044e654800e1321d175451e82e09c5d7b82afe46
abderrahmane <aabderrahmane665@gmail.com>
2024-08-29 10:58:53 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ function Home() {
   return (
     <main className="relative bg-black-100 flex 
     justify-center items-center flex-col 
-    overflow-hidden mx-auto sm:px-10 px-5">
+    mx-auto sm:px-10 px-5 overflow-clip">
 
       <div className="max-w-7xl w-full">
        <FloatingNav navItems={navItems} />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,13 +7,7 @@ const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
+    
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
@@ -24,7 +18,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:contact@jsmastery.pro">
+        <a href="mailto:a.aid@esi-sba.dz">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
